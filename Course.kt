bbe4bf55c74db2b6314acdf333f3309556113cb9
Min-1810 <nhuminnguyenhoang@gmail.com>
2026-03-30 23:06:01 +0700

    Add files via upload

---
+++ Course.kt
@@ -0,0 +1,8 @@
+package com.example.firebase1
+
+data class Course(
+    var courseID: String? = "",
+    var courseName: String? = "",
+    var courseDuration: String? = "",
+    var courseDescription: String? = ""
+)
